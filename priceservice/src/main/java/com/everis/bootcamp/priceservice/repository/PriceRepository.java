0181a0ea5d8c9add14adb9745996e015bd3195b7
Cristian González <cgonzalezhens@gmail.com>
2022-10-24 17:16:25 +0200

    reestructura
    reestructura

--- priceservice/src/main/java/com/everis/bootcamp/priceservice/repository/PriceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.everis.bootcamp.priceservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.everis.bootcamp.priceservice.model.Prices;
-
-public interface PriceRepository extends JpaRepository<Prices, Integer> {
-}
