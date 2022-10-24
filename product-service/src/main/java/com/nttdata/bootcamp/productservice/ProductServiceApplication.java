4cc931b692657050e82ccad9648f48cbf883ac40
Cristian González <cgonzalezhens@gmail.com>
2022-10-24 14:04:39 +0200

    Pequeño cambio para añadir más info.

--- product-service/src/main/java/com/nttdata/bootcamp/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/nttdata/bootcamp/productservice/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package com.nttdata.bootcamp.productservice;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,18 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	//UNA FORMA DE PEDIRLO OBTENDRÍAMOS MÁS INFORMACIÓN
 	@Autowired
 	private HttpServletRequest request;
+	
+	//OTRA FORMA DE PEDIRLO, SOLO VAMOS AL MEOLLO
+	@Value("${server.port}")
+	private String port;
 	
 	@RequestMapping(value = "/port")
 	public String port() {
 
+		//return "El puerto del micro es " + port;
 		return "El puerto del micro es " + request.getServerPort();
 
 	}
